875f40a7de5e3cdc23547b850cb455b023c446c5
Amita Singh Chauhan <singhchauhan.amita@gmail.com>
2015-08-23 21:18:44 +0200

    Initial commit

---
+++ app/src/main/java/com/tea/counter/teacounter/MainActivity.java
@@ -0,0 +1,57 @@
+package com.tea.counter.teacounter;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ShareActionProvider;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+    TextView mainTextView;
+    Button mainButton;
+    EditText mainEditText;
+    ListView mainListView;
+    ArrayAdapter mArrayAdapter;
+    ArrayList mNameList = new ArrayList();
+    ShareActionProvider mShareActionProvider;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 1. Access the TextView defined in layout XML
+        // and then set its text
+        // 2. Access the Button defined in layout XML
+// and listen for it here
+        mainButton = (Button) findViewById(R.id.main_button);
+        mainButton.setOnClickListener(this);
+        mainEditText = (EditText) findViewById(R.id.main_edittext);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        mainTextView.setText(mainEditText.getText().toString()
+                + " is learning Android development!");
+    }
+
+
+}
